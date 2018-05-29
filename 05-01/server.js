42a83499b5cdc3456613d67ba1349d93c76576f5
José Fernando Falcao <jfribeirofalcao@gmail.com>
2018-05-29 06:24:01 -0400

    v5.1-Static serving with Express

---
+++ 05-01/server.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+app.use(express.static(__dirname));
+var server = app.listen(3000, () => {
+    console.log('Server s running on http://localhost:' + server.address().port);
+    
+})
