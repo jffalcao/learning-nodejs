c5edbcec02696780edd17663d20bb5330dd0c31a
José Fernando Falcao <jfribeirofalcao@gmail.com>
2018-05-30 19:50:40 -0400

    v5.3 Create a Get Message Service

--- 05-01/server.js
+++ 05-01/server.js
@@ -2,7 +2,17 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname));
+
+var messages = [
+    {name: 'Tim', message: 'Hello'},
+    {name: 'Jane', message: 'Hi'}
+];
+
+app.get('/messages', (req, res) => {
+    res.send(messages);
+});
+
 var server = app.listen(3000, () => {
-    console.log('Server s running on http://localhost:' + server.address().port);
+    console.log('Server is listening on http://localhost:' + server.address().port);
     
 })
