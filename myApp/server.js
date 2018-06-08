6f78ceccf457cec86ef1c492b435ce228b2e0336
José Fernando Falcao <jfribeirofalcao@gmail.com>
2018-06-08 06:26:57 -0400

    5.4 Create a post messages service complete

--- myApp/server.js
+++ myApp/server.js
@@ -1,7 +1,10 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 
 app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 
 var messages = [
     {name: 'Tim', message: 'Hello'},
@@ -10,6 +13,12 @@ var messages = [
 
 app.get('/messages', (req, res) => {
     res.send(messages);
+});
+
+app.post('/messages', (req, res) => {
+    console.log(req.body);    
+    messages.push(req.body);
+    res.sendStatus(200);
 });
 
 var server = app.listen(3000, () => {
