de6d826369e0b923d488cc0e128705359fbdc818
José Fernando Falcao <jfribeirofalcao@gmail.com>
2018-06-12 06:41:53 -0400

    6.3 Complete - Moongoose

--- myApp/server.js
+++ myApp/server.js
@@ -3,10 +3,13 @@ var bodyParser = require('body-parser');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongoose = require('mongoose');
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+dbUrl = 'mongodb://user:Password!1234@ds255930.mlab.com:55930/learning-node'
 
 var messages = [
     {name: 'Tim', message: 'Hello'},
@@ -26,6 +29,10 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
     console.log('A user connected');    
+});
+
+mongoose.connect(dbUrl, (err) => {
+    console.log('MongoDB connection error: ', err);    
 });
 
 var server = http.listen(3000, () => {
