14705e1fff21712037449d79850796597a2fc2a4
José Fernando Falcao <jfribeirofalcao@gmail.com>
2018-06-17 16:39:56 -0400

    7.3 complete async/await

--- myApp/server.js
+++ myApp/server.js
@@ -24,26 +24,27 @@ app.get('/messages', (req, res) => {
     });
 });
 
-app.post('/messages', (req, res) => {
+app.post('/messages', async (req, res) => {
     var message = new Message(req.body);
 
-    message.save()
-    .then(() => {
-        console.log('saved'); 
-        return Message.findOne({message: 'badword'});
-    })
-    .then(censored => {
-        if(censored) {
-            console.log('censored words found', censored);
-            return Message.remove({_id: censored.id});            
-        };
+    var savedMessage = await message.save();
+    
+    console.log('saved');
+
+    var censored = await Message.findOne({message: 'badword'});
+    if(censored) {
+        await Message.remove({_id: censored.id});            
+    } else {
         io.emit('message', req.body);
-        res.sendStatus(200);
-    })
-    .catch((err) => {
-        res.sendStatus(500);
-        return console.error(err);
-    });
+    }
+
+    res.sendStatus(200); 
+
+//    .catch((err) => {
+//        res.sendStatus(500);
+//        return console.error(err);
+//    });
+
 });
 
 io.on('connection', (socket) => {
